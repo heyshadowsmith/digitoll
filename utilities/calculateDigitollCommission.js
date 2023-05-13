94676e7dcb624d7f927686d8cb2ac25431b0f74e
Shadow Smith <hey@shadowsmith.com>
2023-05-13 01:25:18 -0500

    Fix pricing issue

--- utilities/calculateDigitollCommission.js
+++ utilities/calculateDigitollCommission.js
@@ -1,5 +1,5 @@
 export default function calculateDigitollCommission (digitollPriceInCents) {
-    const tenPercent = digitollPriceInCents * 0.1 // calculate 10% of the price
+    const tenPercent = Math.floor(digitollPriceInCents * 0.1) // calculate 10% of the price
     const total = tenPercent + 30 // add $0.30 to the 10% value
-    return total
+    return Math.round(total)
 }
