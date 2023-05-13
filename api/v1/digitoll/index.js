15a0392aa412cf1390860e070e3af48e9c18b559
Shadow Smith <hey@shadowsmith.com>
2023-05-13 01:04:32 -0500

    Add create Digitoll endpoint

---
+++ api/v1/digitoll/index.js
@@ -0,0 +1,87 @@
+import { PrismaClient } from '@prisma/client'
+import Stripe from 'stripe'
+import getVerifiedDecodedToken from '../../../utilities/getVerifiedDecodedToken'
+import calculateDigitollCommission from '../../../utilities/calculateDigitollCommission'
+
+const stripe = new Stripe(process.env.STRIPE_API_KEY)
+const prisma = new PrismaClient()
+
+export default async (req, res) => {
+    const decodedToken = await getVerifiedDecodedToken(req)
+
+    if (!decodedToken) {
+        res.status(401).json({ messages: ['Unauthorized'] })
+        return
+    }
+
+    if (req.method === 'POST') {
+        const price = req.body.price
+        const destination = req.body.destination
+        const slug = req.body.slug
+        const payoutAccountId = req.body.payoutAccountId
+
+        try {
+            let digitollPrice
+
+            const { data: prices } = await stripe.prices.search({
+                query: `active:'true' AND metadata['price']:'${price}'`,
+            })
+
+            if (prices.length > 0) {
+                digitollPrice = prices[0]
+            } else {
+                digitollPrice = await stripe.prices.create({
+                    currency: 'usd',
+                    unit_amount: price,
+                    product: process.env.STRIPE_PRODUCT_ID,
+                    'metadata[price]': price
+                })
+            }
+
+            const digitollCommission = calculateDigitollCommission(digitollPrice.unit_amount)
+
+            const paymentLink = await stripe.paymentLinks.create({
+                line_items: [
+                  {
+                    price: digitollPrice.id,
+                    quantity: 1,
+                  },
+                ],
+                after_completion: {
+                    type: "redirect",
+                    redirect: {
+                        url: destination
+                    }
+                },
+                application_fee_amount: digitollCommission,
+                transfer_data: {
+                    destination: payoutAccountId,
+                }
+            })
+
+            const digitoll = await prisma.digitoll.create({
+                data: {
+                    payLinkId: paymentLink.id,
+                    slug,
+                    destination,
+                    User: {
+                        connect: {
+                            payoutAccountId
+                        }
+                    }
+                }
+            })
+
+            res.send(digitoll)
+            return
+        } catch (error) {
+            console.error(error)
+            res.status(500)
+
+            return
+        }
+
+    }
+
+    res.status(400).json({ messages: ['Invalid operation'] })
+}
