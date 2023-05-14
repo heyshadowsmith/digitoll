bc5ac2e604012881800b66efb311985e4c5bf3bc
Shadow Smith <hey@shadowsmith.com>
2023-05-13 22:50:05 -0500

    Reconfigure the index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,32 +1,32 @@
 <template>
-    <div v-if="self.onboardingLink"><a :href="self.onboardingLink">Finish setting up your payout details</a></div>
-    <div v-if="self.loginLink"><a :href="self.loginLink">Login to your payout account</a></div>
-    <div v-if="self.editAccountLink"><a :href="self.editAccountLink">Edit your payout account details</a></div>
-    <div>
-        <button v-if="!self.id" @click="auth.authorize({})">Sign In</button>
-        <button v-else @click="store.signOut()">Sign Out</button>
+    <button v-if="!self.id" @click="auth.authorize({})">Sign In</button>
+    <div v-else>
+        <div v-if="self.onboardingLink"><a :href="self.onboardingLink">Finish setting up your payout details</a></div>
+        <div v-if="self.loginLink"><a :href="self.loginLink">Login to your payout account</a></div>
+        <div v-if="self.editAccountLink"><a :href="self.editAccountLink">Edit your payout account details</a></div>
+        <button @click="store.signOut()">Sign Out</button>
+        <p>Signed in as {{ self.email }}.</p>
+        <form @submit.prevent="createDigitoll()">
+            <div>
+                <label for="destination">Destination URL</label>
+                <input type="text" name="destination" v-model="formState.destination">
+            </div>
+            <div>
+                <label for="slug">Slug</label>
+                <input type="text" name="slug" v-model="formState.slug">
+            </div>
+            <div>
+                <label for="price">Price</label>
+                <input type="number" min="100" name="price" v-model="formState.price">
+            </div>
+            <input type="submit" value="Create Digitoll" />
+        </form>
+        <ul>
+            <li v-for="(digitoll, index) in digitolls" :key="index">
+                <a :href="`https://dgtll.link/${digitoll.slug}`" :key="index">https://dgtll.link/{{digitoll.slug}}</a>
+            </li>
+        </ul>
     </div>
-    <p v-if="self.id">Signed in as {{ self.email }}.</p>
-    <form @submit.prevent="createDigitoll()">
-        <div>
-            <label for="destination">Destination URL</label>
-            <input type="text" name="destination" v-model="formState.destination">
-        </div>
-        <div>
-            <label for="slug">Slug</label>
-            <input type="text" name="slug" v-model="formState.slug">
-        </div>
-        <div>
-            <label for="price">Price</label>
-            <input type="number" min="100" name="price" v-model="formState.price">
-        </div>
-        <input type="submit" value="Create Digitoll" />
-    </form>
-    <ul>
-        <li v-for="(digitoll, index) in digitolls" :key="index">
-            <a :href="`https://dgtll.link/${digitoll.slug}`" :key="index">https://dgtll.link/{{digitoll.slug}}</a>
-        </li>
-    </ul>
 </template>
 
 <script setup>
