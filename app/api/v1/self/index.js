90bfa01b97665dbebf9809927905bb8b79c48223
Shadow Smith <hey@shadowsmith.com>
2023-05-18 21:21:29 -0500

    Simplify the self endpoint

--- app/api/v1/self/index.js
+++ app/api/v1/self/index.js
@@ -1,15 +1,13 @@
 import { PrismaClient } from '@prisma/client'
-import Stripe from 'stripe'
 import getVerifiedDecodedToken from '../../../utilities/getVerifiedDecodedToken'
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY)
 const prisma = new PrismaClient()
 
 export default async (req, res) => {
     const decodedToken = await getVerifiedDecodedToken(req)
 
     if (!decodedToken) {
-        res.status(401).json({ messages: ['Unauthorized'] })
+        res.status(401).json({ messages: ['Unauthorized']})
         return
     }
 
@@ -21,34 +19,11 @@ export default async (req, res) => {
                 }
             })
     
-            const account = await stripe.accounts.retrieve(user.payoutAccountId)
-            const finishedOnboarding = account.charges_enabled && account.details_submitted
-            const loginLink = await stripe.accounts.createLoginLink(user.payoutAccountId)
-    
-            let onboardingLink = null
-    
-            if (!finishedOnboarding) {
-                const accountLink = await stripe.accountLinks.create({
-                    account: user.payoutAccountId,
-                    refresh_url: process.env.STRIPE_API_REFRESH_URL,
-                    return_url: process.env.STRIPE_API_RETURN_URL,
-                    type: 'account_onboarding',
-                })
-    
-                onboardingLink = accountLink.url
-            }
-    
-            res.status(200).json({
-                ...user,
-                loginLink: loginLink.url,
-                onboardingLink
-            }) 
-    
+            res.status(200).json(user) 
             return
         } catch (error) {
             console.error(error)
-            res.status(500)
-
+            res.status(500).json({ messages: ['Something unexpected occurred.'] })
             return
         }
     }
